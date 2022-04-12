8dd34073650ecb09b2ca6994e1feb4f6f2c9167b
trowk <trowbridge.iank@gmail.com>
2022-04-12 13:40:14 -0500

    Adding initial files

---
+++ DataPrep/data_prep.py
@@ -0,0 +1,36 @@
+from scipy.io import loadmat
+from sklearn.model_selection import train_test_split
+
+import argparse
+import numpy as np
+import os
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--train_file', type=str, default='S_train.mat',
+        help='Path to training data')
+    parser.add_argument('-d', '--data_store', type=str, default='../Model/Data',
+        help='Directory where data will be stored')
+    parser.add_argument('-s', '--seed', type=int, default=0,
+        help='Random seed used to split the data')
+    parser.add_argument('-v', '--validation_percentage', type=float, default=0.1,
+        help='What percentage of the data that will belong to validation set')
+    args = parser.parse_args()
+
+    matrix_name = (args.train_file.split('/')[-1]).split('.')[0]
+    data = loadmat(args.train_file)[matrix_name]
+
+    x_train, x_test, _, _ = train_test_split(data, np.zeros(data.shape[0]), test_size=args.validation_percentage, random_state=args.seed)
+    if not os.path.isdir(args.data_store):
+        os.makedirs(args.data_store)
+    train_file = os.path.join(args.data_store, 'train.npy')
+    test_file = os.path.join(args.data_store, 'test.npy')
+    with open(train_file, 'wb') as f:
+        np.save(f, x_train)
+    with open(test_file, 'wb') as f:
+        np.save(f, x_test)
+    
+
+
+if __name__ == "__main__":
+    main()
